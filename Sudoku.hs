01ecbcacc03fe82720d5bc6d5d1d68063da6aac7
Jizhou-Che <che_mail@icloud.com>
2020-01-29 03:19:13 +0000

    Sudoku initiated.

---
+++ Sudoku.hs
@@ -0,0 +1,35 @@
+import Data.List
+
+type Grid = Matrix Value
+type Matrix a = [Row a]
+type Row a = [a]
+type Value = Char
+
+boxsize :: Int
+boxsize = 3
+
+values :: [Value]
+values = ['1'..'9']
+
+empty :: Value -> Bool
+empty = (== '.')
+
+single :: [a] -> Bool
+single [_] = True
+single _ = False
+
+rows :: Matrix a -> [Row a]
+rows = id
+
+cols :: Matrix a -> [Row a]
+cols = transpose
+
+boxs :: Matrix a -> [Row a]
+boxs = unpack . map cols . pack
+       where pack = split . map split
+             split = chop boxsize
+             unpack = map concat . concat
+
+chop :: Int -> [a] -> [[a]]
+chop n [] = []
+chop n xs = take n xs : chop n (drop n xs)
